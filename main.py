9c0728ef72cb59bf74fcd5a24b7f48f75d472ba7
Pavlov Ivan <ISerPavlov@sberbank.ru>
2021-12-27 03:51:11 +0300

    Changed get_messages_in_chat method and corrected response codes

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 
-from flask import Flask, request, jsonify
+from flask import Flask
 from flask_cors import CORS
 
-from allfunc import db
-from routes import main_blueprint
+from data.allfunc import db
+from routes.routes import main_routes
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///webchat.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data///webchat.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 CORS(app, resources={r'/*': {'origins': '*'}})
 
@@ -15,5 +15,5 @@ if __name__ == '__main__':
     db.init_app(app)
     db.app = app
     db.create_all()
-    app.register_blueprint(main_blueprint)
+    app.register_blueprint(main_routes)
     app.run(host="localhost", port=5000, debug=True)
